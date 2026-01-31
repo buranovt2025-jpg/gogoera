e4117092b34f24fbb41bd8af472ca76c4fbf806f
buranovt2025-jpg <buranovt2025-jpg@users.noreply.github.com>
2026-01-31 06:16:18 +0500

    Fix: cool_dropdown DropdownController - remove type args for 2.1.0 compatibility

--- lib/utiles/CoustomWidget/Page_devided/add_to_cart_bottom_sheet.dart
+++ lib/utiles/CoustomWidget/Page_devided/add_to_cart_bottom_sheet.dart
@@ -30,7 +30,7 @@ class AddToCartBottomSheet extends StatelessWidget {
   AddProductToCartController addProductToCartController = Get.put(AddProductToCartController());
   RemoveAllProductFromCartController removeAllProductFromCartController = Get.put(RemoveAllProductFromCartController());
 
-  Map<String, DropdownController<String>> dropdownControllers = {};
+  Map<String, DropdownController> dropdownControllers = {};
   bool areAllAttributesFilled = false;
 
   @override
@@ -57,7 +57,7 @@ class AddToCartBottomSheet extends StatelessWidget {
                               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
                               child: Column(
                                 children: userProductDetailsController.selectedValuesByType.keys.map((key) {
-                                  DropdownController<String> dropdownController = dropdownControllers[key] ?? DropdownController<String>();
+                                  DropdownController dropdownController = dropdownControllers[key] ?? DropdownController();
                                   userProductDetailsController.categoryDropdownItems =
                                       userProductDetailsController.selectedValuesByType[key]!.map((value) {
                                     return CoolDropdownItem<String>(
